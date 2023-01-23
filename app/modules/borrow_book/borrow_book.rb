cf6a294bce9c2c8d59e7c5512236a8f77d2048fa
Rafal Piekara <rafal@piekara.me>
2023-01-23 11:52:15 +0100

    Example with flexible public api

--- app/modules/borrow_book/borrow_book.rb
+++ app/modules/borrow_book/borrow_book.rb
@@ -1,5 +1,8 @@
 module Book
   class BorrowBook
 
+    def self.call(book_id, user, other_param)
+      # Borrow book logic
+    end
   end
 end
--- app/modules/borrow_book/public/api.rb
+++ app/modules/borrow_book/public/api.rb
@@ -4,6 +4,8 @@ module BorrowBook
 
       def self.borrow_book(book_id, user)
         # Borrow book logic
+        other_params = 'Another param needed to borrow this book'
+        BorrowBook::BorrowBook.call(book_id, user, other_param)
       end
     end
   end
